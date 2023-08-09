6720da47d0b5758a588d3a792172156aa51cf70b
Jacopo Cirica <ciricaj@cardiff.ac.uk>
2023-08-09 21:56:43 +0200

    update35

--- client/script.js
+++ client/script.js
@@ -66,7 +66,7 @@ const handleSubmit = async (e) => {
     e.preventDefault()
 
     const data = new FormData(form)
-    console.log(chatContainer)
+    chatContainer.forEach((element) => console.log(element));
 
     // user's chatstripe
     chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
