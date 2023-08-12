5a7925023536305da36e129ba9015a4010dc20a7
Jacopo Cirica <ciricaj@cardiff.ac.uk>
2023-08-12 18:00:40 +0200

    update81

--- client/script.js
+++ client/script.js
@@ -7,6 +7,7 @@ const chatContainer = document.querySelector('#chat_container')
 let loadInterval
 let userID=''
 let userEmail= false
+let userName= false
 
 function loader(element) {
     element.textContent = ''
@@ -91,16 +92,16 @@ function firstSubmit(){
 
     var firstmessage=data.get('prompt')
     var phrases = [
-        "Thank you very much! now could you tell me your name?",
-        "I like your email! Can I know your name too? ",
-        "Thank you, now how about telling me your name?",
-        "Perfect, do you even have a name?",
-        "Great, thank you. Could you also type your name?",
-        "Got it, what's your name? ",
-        "I love your email, would you also have a name I can call you?",
+        "Thank you very much! now could you tell me your full, legal name?",
+        "I like your email! Can I know your legal name too? ",
+        "Thank you, now how about telling me your full name?",
+        "Perfect, do you even have a legal name?",
+        "Great, thank you. Could you also type your full, legal name?",
+        "Got it, what's your name? I can help you better if you give me your full, legal name.",
+        "What’s your name? I can help you better if you give me your full, legal name.",
         "You have been very kind! Now could you also give me your name?",
-        "Thank you, tell me more about yourself. Do you have a name? ",
-        "The first step is taken, do you have a name? what shall I call you?"
+        "Thank you, tell me more about yourself. Do you have a name? I could help you better! ",
+        "The first step is taken, do you have a full legal name? what shall I call you?"
     ];
     var phrases1 = [
         "You have not entered a valid email, can you repeat?",
@@ -139,6 +140,75 @@ function firstSubmit(){
     } else {
         var randomIndex = Math.floor(Math.random() * phrases1.length); // Genera un indice casuale nell'intervallo dell'array
         var selectedPhrase = phrases1[randomIndex]; // Sceglie una frase casuale dall'array
+        setTimeout(function() {
+            clearInterval(loadInterval)
+            messageDiv.innerHTML = " "
+            typeText(messageDiv, selectedPhrase);
+        }, 2500);
+    }
+
+}
+function secondSubmit(){
+    const data = new FormData(form)
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+    form.reset()
+        // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+    
+        // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+    
+        // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+    
+        // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    var secondmessage=data.get('prompt')
+    var phrases2 = [
+        "Nice to meet you! Can you tell me a little bit about yourself, your work, and why you’re joining the workshop today?",
+        "Such a pleasure to meet you! Might you share a little about who you are, what you do professionally, and what has driven you to be a part of today's workshop?",
+        "This is a nice introduction! Could you offer some insights into your background, your line of work, and the reasons that have led you to join the workshop on this day?",
+        "I'm glad to meet you! Can you give me a short glimpse into your life, both personally and professionally, and shed light on what prompted your involvement in this workshop?",
+        "It's delightful meeting you! Would you mind sharing a brief overview of your personal and professional life, as well as your motivations for attending this workshop?",
+        "A warm welcome to you! Might you provide a small introduction about yourself, your occupation, and the purpose driving your attendance at today's workshop?",
+        
+    ];
+    var phrases3 = [
+        "Is that your name? could you kindly type it again?",
+        "OK, could you please enter a valid full name?",
+        "I do not read any legal name, could you please try again? ",
+        "Are you sure you have entered your full name? Why don't you try again?",
+        "Mmm, before we start our conversation I would need your email. Could you type it in?",
+        "Forgive me, perhaps I did not explain myself well. Could you please enter yout full, legal name? it will help me to converse better with you!",
+        
+    ];
+    if (secondmessage.length>3) {
+        userName=true
+        var randomIndex = Math.floor(Math.random() * phrases2.length); // Genera un indice casuale nell'intervallo dell'array
+        var selectedPhrase = phrases2[randomIndex]; // Sceglie una frase casuale dall'array
+        
+        setTimeout(function() {
+            clearInterval(loadInterval)
+            messageDiv.innerHTML = " "
+            typeText(messageDiv, selectedPhrase);
+        }, 2500);
+        fetch('https://chatbot-test-zd9j.onrender.com/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                prompt: secondmessage,
+                previous_message: 'jacopo',
+                userID: userID
+            })
+        })
+        
+    } else {
+        var randomIndex = Math.floor(Math.random() * phrases3.length); // Genera un indice casuale nell'intervallo dell'array
+        var selectedPhrase = phrases3[randomIndex]; // Sceglie una frase casuale dall'array
         setTimeout(function() {
             clearInterval(loadInterval)
             messageDiv.innerHTML = " "
@@ -216,10 +286,12 @@ const handleSubmit = async (e) => {
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Evita il comportamento predefinito del form
 
-    if (userEmail===true) {
+    if (userName===true) {
         // Fai qualcosa qui se la condizione è verificata
         handleSubmit(event);
-    } else {
+    } else if(userEmail===true){
+        secondSubmit()
+    }else{
         firstSubmit()
     }
 });
@@ -227,10 +299,12 @@ form.addEventListener('submit', function(event) {
 
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
-        if(userEmail===true){
+        if(userName===true){
         handleSubmit(e)
+    }else if (userEmail===true){
+        secondSubmit() 
     }else{
-        firstSubmit() 
+        firstSubmit()
     }
 }
 })
